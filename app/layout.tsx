7d43122697d1987271c141bbce20989ee0bb1e97
Nabil faturrahman <nabilfaturr44@gmail.com>
2023-12-26 15:32:08 +0700

    feat: finish setting up clerk for authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,10 +19,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} flex h-screen flex-col`}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} flex h-screen flex-col`}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
