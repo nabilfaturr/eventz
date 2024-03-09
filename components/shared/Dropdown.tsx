ab8da9283588c31a12ef0bfbea4092105a93b669
Nabil faturrahman <nabilfaturr44@gmail.com>
2024-03-09 11:27:35 +0700

    feat: create pop up new category ui

--- components/shared/Dropdown.tsx
+++ components/shared/Dropdown.tsx
@@ -8,22 +8,30 @@ import {
 } from "@/components/ui/select";
 import { ICategory } from "@/lib/database/models/category.model";
 
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Input } from "../ui/input";
+
 export type DropdownProps = {
   value?: string;
   handleChange: () => void;
 };
 
 const Dropdown = ({ value, handleChange }: DropdownProps) => {
-  const [categories, setCategories] = useState<ICategory[]>([
-    {
-      _id: "1",
-      name: "Technology",
-    },
-    {
-      _id: "1",
-      name: "Finance",
-    },
-  ]);
+  const [categories, setCategories] = useState<ICategory[]>([]);
+  const [newCategory, setNewCategory] = useState("");
+
+  const handleAddCategory = () => {};
+
   return (
     <Select>
       <SelectTrigger className="select-field">
@@ -40,6 +48,30 @@ const Dropdown = ({ value, handleChange }: DropdownProps) => {
               {category.name}
             </SelectItem>
           ))}
+        <AlertDialog>
+          <AlertDialogTrigger className="p-medium-14 flex w-full rounded-sm py-3 pl-8 text-primary-500 hover:bg-primary-50 focus:text-primary-500">
+            Open
+          </AlertDialogTrigger>
+          <AlertDialogContent className="bg-white">
+            <AlertDialogHeader>
+              <AlertDialogTitle>New Category</AlertDialogTitle>
+              <AlertDialogDescription>
+                <Input
+                  type="text"
+                  placeholder="Category Name"
+                  className="input-field mt-3 text-black"
+                  onChange={(e) => setNewCategory(e.target.value)}
+                />
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction onClick={() => handleAddCategory}>
+                Add
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </SelectContent>
     </Select>
   );
